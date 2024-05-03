8d4f4f8a2fb65f9dcbafe1431b9d7dd9b6535661
Tyler Holland <tytaniumdev@gmail.com>
2024-05-03 15:45:53 -0700

    Add offspec pulling prioritization
    Refactored functions to make this not just one giant block of code.
    
    Added more debugging local print statements.
    
    Improved the logic for moving people around to fill groups. It not follows a logical path as if we were manually making groups inside of WoW. It's not algorithmically optimal, but it's much easier to follow and will be easier to modify in the future.

--- bot.py
+++ bot.py
@@ -33,57 +33,19 @@ async def showShortTyping(channel):
 def dashed(name):
      return '?' * len(name)
 
+
+
 @bot.command()
 async def test(ctx):
     await coreWheel(ctx, debug = True)
 
 @bot.command()
-async def testGreedy(ctx):
-    await coreWheel(ctx, debug = True, useOffspecInPartialGroup = True)
-
-@bot.command()
 async def wheel(ctx):
-    await coreWheel(ctx = ctx, useOffspecInPartialGroup = False)
+    await coreWheel(ctx = ctx)
 
-@bot.command()
-async def greedyWheel(ctx):
-    await coreWheel(ctx = ctx, useOffspecInPartialGroup = True)
-
-
-async def coreWheel(ctx, debug: bool = None, useOffspecInPartialGroup: bool = None):
-    debug = False if debug is None else debug
-    useOffspecInPartialGroup = False if useOffspecInPartialGroup is None else useOffspecInPartialGroup
-
-    # Set up a list for each role
-    tanks = []
-    healers = []
-    dps = []
-
-    offtanks = []
-    offhealers = []
-    offdps = []
-
-    def removeFromRoleLists(member):
-         tanks.remove(member)
-         healers.remove(member)
-         dps.remove(member)
-         offtanks.remove(member)
-         offhealers.remove(member)
-         offdps.remove(member)
-
-    # Save a reference to the channel the command was typed in
-    channel = ctx.channel
-
-    # Grab all members in the channel the message was sent in
-    if debug:
-        # Testing Code
-        testChannel = discord.utils.get(ctx.guild.channels, name='path-of-exile')
-        members = [member for member in testChannel.members if member.bot == False]
-    else:
-        members = [member for member in ctx.channel.members if member.bot == False]
-
-    # Add each member to one of the primary role buckets, in Tank > Healer > DPS priority
+def fillRoleLists(members, tanks, healers, dps, offtanks, offhealers, offdps):
     for member in members:
+        # Add each member to one of the primary role buckets, in Tank > Healer > DPS priority
         roles = [role.name for role in member.roles]
         if 'Tank' in roles:
             tanks.append(WoWName(member))
@@ -100,81 +62,187 @@ async def coreWheel(ctx, debug: bool = None, useOffspecInPartialGroup: bool = No
             offhealers.append(WoWName(member))
         if 'DPS Offspec' in roles:
             offdps.append(WoWName(member))
+
+
+async def coreWheel(ctx, debug: bool = None):
+    debug = False if debug is None else debug
     
+    # Save a reference to the channel the command was typed in
+    channel = ctx.channel
 
-    # Debugging help
-    if(debug):
-        print(f'Tanks: {tanks}')
-        print(f'Healers: {healers}')
-        print(f'DPS: {dps}')
-        print(f'Offtanks: {offtanks}')
-        print(f'Offhealers: {offhealers}')
-        print(f'OffDPS: {offdps}')
+    # Set up a list for each role
+    tanks = []
+    healers = []
+    dps = []
 
-    # Sort everyone into groups of 5, with 1 tank, 1 healer, and 3 dps.
+    offtanks = []
+    offhealers = []
+    offdps = []
+
+
+    # Get the members of the channel we want to use to fill the roles
+    if debug:
+        # Testing Code
+        testChannel = discord.utils.get(ctx.guild.channels, name='path-of-exile')
+        members = [member for member in testChannel.members if member.bot == False]
+    else:
+        members = [member for member in channel.members if member.bot == False]
+
+    fillRoleLists(members, tanks, healers, dps, offtanks, offhealers, offdps)
+
+    print(f'Tanks: {tanks}')
+    print(f'Healers: {healers}')
+    print(f'DPS: {dps}')
+    print(f'Offtanks: {offtanks}')
+    print(f'Offhealers: {offhealers}')
+    print(f'OffDPS: {offdps}')
+
+    # Figure out how many groups we'll have, and what the size of our partial
+    # group would be for use later on.
+    totalPlayers = min((len(tanks) + len(healers) + len(dps)), len(members))
+    numberOfFullGroups = totalPlayers // 5
+    partialGroupSize = totalPlayers % 5
+    print(f'Should be able to make {numberOfFullGroups} full groups')
+    print(f'Partial group will be {partialGroupSize} people')
+
+    # First pass: Check to see if we have too many of any role. If we do, we 
+    # should assign that number of extras to offspecs where they are needed.
+    def recalculateExtras():
+        nonlocal extraTanks
+        nonlocal extraHealers
+        nonlocal extraDPS
+        extraTanks = len(tanks)-numberOfFullGroups
+        extraHealers = len(healers)-numberOfFullGroups
+        extraDPS = len(dps)-(numberOfFullGroups*3)
+        print(f'We have {extraTanks} extra tanks')
+        print(f'We have {extraHealers} extra healers')
+        print(f'We have {extraDPS} extra dps')
+
+    extraTanks = 0
+    extraHealers = 0
+    extraDPS = 0
+    recalculateExtras()
+
+    # Do a first pass where we just reassign any "extra" people to offspec roles
+    # where they're needed.
     #
-    # First we need to sanitize the tank, heal, and dps lists to have things
-    # be balanced in the 1-1-3 ratio.
-     
-    numberOfGroupsToPullOffspecInto = len(members) // 5
-    if useOffspecInPartialGroup:
-        numberOfGroupsToPullOffspecInto += 1
+    # This does not cover the weird case where we have:
+    # 0 tanks, 2 healers, 8 dps
+    # Only healers offspec tank
+    # Dps only offspecs healing
+    # In this situation, nobody would get reassigned to tanks because there 
+    # weren't "extra" healers.
+    print('\nFirst pass - moving extras around\n')
 
-    # Make sure we have enough tanks, healers, and dps in that order of importance.
-    # If we don't have enough, remove someone from the appropriate offspec list.
+    # Fill in tanks with extra people
+    if extraTanks < 0 and extraHealers > 0:
+        # We have extra healers, so pull from that group first
+        pullOffspecFromGroup(numberToPull=min(abs(extraTanks), extraHealers), fromGroup=healers, offspecGroup=offtanks, toGroup=tanks)
+        recalculateExtras()
+    if extraTanks < 0 and extraDPS > 0:
+         # We have extra dps, so pull from that group next
+        pullOffspecFromGroup(numberToPull=min(abs(extraTanks), extraDPS), fromGroup=dps, offspecGroup=offtanks, toGroup=tanks)
+        recalculateExtras()
 
-    haveEnoughMainSpecTanks = len(tanks) >= numberOfGroupsToPullOffspecInto
-    haveEnoughMainSpecHealers = len(healers) >= numberOfGroupsToPullOffspecInto
-    haveEnoughMainSpecDPS = len(dps) >= numberOfGroupsToPullOffspecInto
+    # Fill in healers with extra people
+    if extraHealers < 0 and extraTanks > 0:
+        # We have extra tanks, so pull from that group first
+        pullOffspecFromGroup(numberToPull=min(abs(extraHealers), extraTanks), fromGroup=tanks, offspecGroup=offhealers, toGroup=healers)
+        recalculateExtras()
+    if extraHealers < 0 and extraDPS > 0:
+         # We have extra dps, so pull from that group next
+        pullOffspecFromGroup(numberToPull=min(abs(extraHealers), extraDPS), fromGroup=dps, offspecGroup=offhealers, toGroup=healers)
+        recalculateExtras()
 
-    # First try to pull offspec DPS into the tank role
-    if not haveEnoughMainSpecTanks:
-        print('Filling tanks with dps')
-        # Start with filling the tank role with dps
-        availableDPSToTank = list(set(dps).intersection(offtanks))
-        print(f'Available DPS to tank: {availableDPSToTank}')
-        if availableDPSToTank:
-            # Move random DPS into the tank role
-            random.shuffle(availableDPSToTank)
-            for x in range(numberOfGroupsToPullOffspecInto - len(tanks)):
-                if availableDPSToTank:
-                    tankingDPS = availableDPSToTank.pop()
-                    tanks.append(tankingDPS)
-                    dps.remove(tankingDPS)
-                    print(f'Assigning {tankingDPS} from dps to tank')
-                else:
-                    break
+    # Fill in dps with extra people
+    if extraDPS < 0 and extraHealers > 0:
+        # We have extra healers, so pull from that group first
+        pullOffspecFromGroup(numberToPull=min(abs(extraDPS), extraHealers), fromGroup=healers, offspecGroup=offdps, toGroup=dps)
+        recalculateExtras()
+    if extraDPS < 0 and extraTanks > 0:
+         # We have extra tanks, so pull from that group next
+        pullOffspecFromGroup(numberToPull=min(abs(extraDPS), extraTanks), fromGroup=tanks, offspecGroup=offdps, toGroup=dps)
+        recalculateExtras()
 
-    # Next try to pull offspec DPS into the healer role
-    if not haveEnoughMainSpecHealers:
-        print('Filling healers with dps')
-        # Start with filling the healer role with dps
-        availableDPSToHeal = list(set(dps).intersection(offhealers))
-        print(f'Available DPS to heal: {availableDPSToHeal}')
-        if availableDPSToHeal:
-            # Move random DPS into the healer role
-            random.shuffle(availableDPSToHeal)
-            for x in range(numberOfGroupsToPullOffspecInto - len(healers)):
-                if availableDPSToHeal:
-                    healingDPS = availableDPSToHeal.pop()
-                    healers.append(healingDPS)
-                    dps.remove(healingDPS)
-                    print(f'Assigning {healingDPS} from dps to heals')
-                else:
-                    break
+    print(f'\nAfter first pass, we have {len(tanks)} tanks, {len(healers)} healers, {len(dps)} dps\n')
+   
+    # Do a second pass where we force people out of main spec roles to fill in
+    # gaps. This handles the above weird case with the 0 tanks, 2 healers, 8 dps.
+    for x in range(2):
+        # Fill in tanks
+        if extraTanks < 0:
+            print('Second round pulling healer to tank')
+            # We have extra healers, so pull from that group first
+            pullOffspecFromGroup(numberToPull=abs(extraTanks), fromGroup=healers, offspecGroup=offtanks, toGroup=tanks)
+            recalculateExtras()
 
-    # Check again to see if we now have enough tanks and healers
-    haveEnoughTanks = len(tanks) >= numberOfGroupsToPullOffspecInto
-    haveEnoughHealers = len(healers) >= numberOfGroupsToPullOffspecInto
+            print('Second round pulling dps to tank')
+            # We have extra dps, so pull from that group next
+            pullOffspecFromGroup(numberToPull=abs(extraTanks), fromGroup=dps, offspecGroup=offtanks, toGroup=tanks)
+            recalculateExtras()
 
-    print(f'Have enough tanks? {haveEnoughTanks}. Have enough healers? {haveEnoughHealers}')
+        # Fill in healers
+        if extraHealers < 0:
+            print('Second round pulling tank to heal')
+            # We have extra tanks, so pull from that group first
+            pullOffspecFromGroup(numberToPull=abs(extraHealers), fromGroup=tanks, offspecGroup=offhealers, toGroup=healers)
+            recalculateExtras()
+
+            print('Second round pulling dps to heal')
+            # We have extra dps, so pull from that group next
+            pullOffspecFromGroup(numberToPull=abs(extraHealers), fromGroup=dps, offspecGroup=offhealers, toGroup=healers)
+            recalculateExtras()
+
+        # Fill in dps
+        if extraDPS < 0:
+            print('Second round pulling heal to dps')
+            # We have extra healers, so pull from that group first
+            pullOffspecFromGroup(numberToPull=abs(extraDPS), fromGroup=healers, offspecGroup=offdps, toGroup=dps)
+            recalculateExtras()
+
+            print('Second round pulling tank to dps')
+            # We have extra tanks, so pull from that group next
+            pullOffspecFromGroup(numberToPull=abs(extraDPS), fromGroup=tanks, offspecGroup=offdps, toGroup=dps)
+            recalculateExtras()
+        print(f'\nAfter second pass round {x}, we have {len(tanks)} tanks, {len(healers)} healers, {len(dps)} dps\n')
+
+    await printGroups(ctx, channel, tanks, healers, dps)
 
 
+# Pulls one user from one group and puts them in another, as long as they're 
+# also part of the offspec group.
+#
+# For example, to pull from DPS and into the Tank role, the arguments would be:
+#    fromGroup: dps
+#    offspecGroup: offtanks
+#    toGroup: tanks
+#
+# This will result in a random dps that has the offtank role into the tank
+# group, and remove them from the dps group.
+def pullOffspecFromGroup(numberToPull: int, fromGroup: list, offspecGroup: list, toGroup: list):
+    availableToPull = list(set(fromGroup).intersection(offspecGroup))
+    print(f'Available users to pull: {availableToPull}')
+    if availableToPull:
+        # Move random user into the to group
+        random.shuffle(availableToPull)
+        for x in range(numberToPull):
+            if availableToPull:
+                pulledUser = availableToPull.pop()
+                toGroup.append(pulledUser)
+                fromGroup.remove(pulledUser)
+                print(f'Assigning {pulledUser} to new group')
+            else:
+                break
 
 
-
-
-    # Finally, we want these groups to be roughly random, so we'll:
+# Prints out groups with the given tanks, healers, and dps to one Discord embed
+# message per group.
+#
+# Does not have any fancy logic to arrange groups, just does the Discord embed
+# sending. The tanks, healers, and dps should be the exact roles each of those
+# users will fill.
+async def printGroups(ctx, channel, tanks, healers, dps):
+    # We want these groups to be roughly random, so we'll:
     # 1) Shuffle all of the primary role lists
     # 2) Pop 1 tank, 1 healer, and 3 dps from the respective lists
     # 3) Print out that group to the channel
